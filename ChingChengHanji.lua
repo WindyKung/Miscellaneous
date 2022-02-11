9295a146af7874b1a78b1507c138ad36bdadd217
WindyKung <86519657+WindyKung@users.noreply.github.com>
2022-02-11 22:07:30 +0700

    Update ChingChengHanji.lua

--- ChingChengHanji.lua
+++ ChingChengHanji.lua
@@ -1,11 +1,11 @@
-local sex = Instance.new("Sound")
-sex.Parent = game:GetService("SoundService")
-sex.SoundId = "rbxassetid://5648499584"
-sex.Volume = 5
-sex:Play()
-wait(0.1)
-game.Players.LocalPlayer:Kick("\nChing Cheng Hanji")
-wait(0.2)
-while true do
+local sex = Instance.new("Sound") --Create New Instance With Class = "Sound"
+sex.Parent = game:GetService("SoundService") --Parent Sound Instance That It Just Create To "SoundService"
+sex.SoundId = "rbxassetid://5648499584" --Change Sound ID To "rbxassetid://5648499584"
+sex.Volume = 5 --Change Sound Volume To 5
+sex:Play() --Play The Sound
+wait(0.1) --Wait For 0.1 Second
+game.Players.LocalPlayer:Kick("\nChing Cheng Hanji") --Kick LocalPlayer With Reason Of "\nChing Cheng Hanji" ("\n" = New Line)
+wait(0.2) --Wait For 0.2 Second
+while true do --Loop With No Delay To Freeze Game 
     print("uwu")
 end
